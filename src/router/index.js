c3d5ecb129bb5dbb469ee804943407c25cf48553
lrh21g <837233792@qq.com>
2021-03-10 11:39:02 +0800

    登陆注册

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,36 @@ const routes = [
     name: 'Home',
     component: () => import(/* webpackChunkName: "home" */ '../views/home/Home'),
   },
+  {
+    path: '/register',
+    name: 'Register',
+    component: () => import(/* webpackChunkName: "register" */ '../views/register/Register'),
+    beforeEnter(to, from, next) {
+      const { isLogin } = localStorage;
+      isLogin ? next({ name: 'Home' }) : next();
+    },
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/login/Login'),
+    beforeEnter(to, from, next) {
+      const { isLogin } = localStorage;
+      isLogin ? next({ name: 'Home' }) : next();
+    },
+  },
 ];
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  const { isLogin } = localStorage;
+  const { name } = to;
+  const isLoginOrRegister = name === 'Login' || name === 'Register';
+  isLogin || isLoginOrRegister ? next() : next({ name: 'Login' });
 });
 
 export default router;
